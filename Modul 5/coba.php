032fd179c913c2fee433e503826d1e3415b23493
Will <willy.rafaelfs@gmail.com>
2025-05-08 20:17:17 +0700

    Added Modul 5

---
+++ Modul 5/coba.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Willy Rafael F. Silalahi First PHP</title>
+</head>
+
+<body>
+  <?php
+  echo "tulisan ini akan keluar karena echo";
+  <p>apakah tulisan ini akan keluar</p>
+  ?>
+  <script src="" async defer></script>
+</body>
+</html>
