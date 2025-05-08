5f3261d15f0de7a1ad34d275435ef07480c2db9d
Will <willy.rafaelfs@gmail.com>
2025-05-09 05:58:31 +0700

    Update cetak_string.php, modul 5

--- Modul 5/cetak_string.php
+++ Modul 5/cetak_string.php
@@ -1,3 +1,7 @@
 <?php
-echo "Saya telah belajar PHP, Mydreamisprogrammer, 123123123123123"
+echo "Nama: Willy Rafael F. Silalahi<br>";
+echo "NIM: 23083000168<br>";
+echo "Kelas: 4G<br>";
+echo "Alamat: Kedungkandand, Kota Malang<br>";
+echo "Email: 23083000168@student.unmer.ac.id<br>";
 ?>
